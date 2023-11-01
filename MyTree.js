f3778ed4a92b1b8726df68b96c5f6d9f8396677b
German Vassiljev <93324304+GermanVassiljev@users.noreply.github.com>
2023-11-01 12:50:31 +0200

    Add files via upload

--- MyTree.js
+++ MyTree.js
@@ -3,7 +3,7 @@ import Tree from 'react-d3-tree';
 
 const MyTree = ({ data }) => {
     return (
-        <div style={{ width: '100%', height: '800px', border: '2pt solid Black'}}>
+        <div style={{ width: '100%', height: '800px', border: '2pt solid #01385E'}}>
             <Tree data={data} orientation='vertical'/>
         </div>
     );
